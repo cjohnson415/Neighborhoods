30da0a10730cfa688faca413d1fbbeab5b9e5631
Charlie Johnson <cjohnson415@gmail.com>
2012-11-14 17:32:37 -0800

    Scripts to collect data and to process it

---
+++ categories.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var async = require('async');
+
+function getCats() {
+  fs.readFile('POSTS_MEDIUM.json', null, function(err, data) {
+    posts = JSON.parse(data);
+    var categories = {};
+    var seen = 0;
+    var dirty = 0;
+    var uniques = 0;
+    async.forEach(posts, function(post, callback) {
+      seen++;
+      var loc = post.location;
+      if (!loc.metroCode) dirty++;
+      else if (!(metroCodes[loc.metroCode])) {
+        metroCodes[loc.metroCode] = 1;
+        uniques++;
+      } else metroCodes[loc.metroCode]++;
+      callback();
+    }, function(err) {
+      console.log('done');
+      dirtyPercent = dirty / seen;
+      results = {uniques:uniques, seen: seen, percent_dirty: dirtyPercent, metroCodes: metroCodes};
+      fs.writeFile('METRO_CODES.json', JSON.stringify(results, null, 2), function(err) {
+        if (err) console.log(err);
+        console.log("It's saved!");
+      });
+    });
+  });
+}
+
+getCodes();
