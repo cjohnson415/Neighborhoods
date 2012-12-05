8ffcd4ecceb9d2613a1b17a5d01bd984b3c23aff
Michael Kim <mpkim@stanford.edu>
2012-12-04 19:45:27 -0800

    parsed posts into a CSV for each category where each post is a separate entry.  CITY_CODE, title, body.  The bodies had any tail that Craigslist adds onto them removed

--- json_files/catsplit.py
+++ json_files/catsplit.py
@@ -1,19 +1,19 @@
 from json import load, dumps
 
 with open('POSTS_MEDIUM.json', 'rb') as source:
-	data = load(source)
+   data = load(source)
 
 posts_by_cat = {}
 
 for post in data:
-	code = post['categoryClassName']
-	print code
-	if code:
-		if code not in posts_by_cat:
-			posts_by_cat[code] = []
-		posts_by_cat[code].append(post)
+   code = post['categoryClassName']
+   print code
+   if code:
+      if code not in posts_by_cat:
+         posts_by_cat[code] = []
+      posts_by_cat[code].append(post)
 
 for code,posts in posts_by_cat.iteritems():
-	filename = "posts_by_cat/POSTS_%s.json"%code
-	with open(filename, 'wb') as outfile:
-		outfile.write(dumps(posts))
+   filename = "posts_by_cat/POSTS_%s.json"%code
+   with open(filename, 'wb') as outfile:
+      outfile.write(dumps(posts))
